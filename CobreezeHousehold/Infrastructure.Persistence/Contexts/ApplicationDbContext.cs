c2068dd4f457dd1353c282a9b20753bedcd79e3a
Tybo Vanderstraeten <vanderstraeten.tybo@hotmail.com>
2021-01-10 10:56:57 +0100

    feat: Add basic infrastructure.persistence

---
+++ CobreezeHousehold/Infrastructure.Persistence/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence.Contexts
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<HouseholdGroup> Households { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ CobreezeHousehold/Infrastructure.Persistence/ServiceExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Application.Interfaces;
+using Infrastructure.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Persistence
+{
+    public static class ServiceExtensions
+    {
+        public static void AddPersistenceLayer(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection"),
+                    builder => builder.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName));
+            });
+
+            services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+        }
+    }
+}
