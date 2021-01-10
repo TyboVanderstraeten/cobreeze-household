533a43198d59a53971ef1c15fcf778a0c1eca21c
Tybo Vanderstraeten <vanderstraeten.tybo@hotmail.com>
2021-01-10 12:33:26 +0100

    feat: Add usercontroller

--- CobreezeHousehold/Presentation.WebApi/Controllers/v1/UserController.cs
+++ CobreezeHousehold/Presentation.WebApi/Controllers/v1/UserController.cs
@@ -1,9 +1,71 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application.Features.UserFeatures.Commands;
+using Application.Features.UserFeatures.Queries;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace Presentation.WebApi.Controllers.v1
 {
-    [ApiVersion]
-    public class UserController
+    [ApiVersion("1.0")]
+    public class UserController : BaseApiController
     {
+        /// <summary>
+        /// Gets all Users.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await Mediator.Send(new GetAllUsersQuery()));
+        }
+
+        /// <summary>
+        /// Gets User Entity by Id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return Ok(await Mediator.Send(new GetUserByIdQuery { Id = id }));
+        }
+
+        /// <summary>
+        /// Creates a New User.
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateUserCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+
+        /// <summary>
+        /// Updates the User Entity based on Id.   
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        [HttpPut("[action]")]
+        public async Task<IActionResult> Update(int id, UpdateUserCommand command)
+        {
+            if (id != command.Id)
+            {
+                return BadRequest();
+            }
+
+            return Ok(await Mediator.Send(command));
+        }
+
+        /// <summary>
+        /// Deletes User Entity based on Id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return Ok(await Mediator.Send(new DeleteUserByIdCommand { Id = id }));
+        }
     }
 }
