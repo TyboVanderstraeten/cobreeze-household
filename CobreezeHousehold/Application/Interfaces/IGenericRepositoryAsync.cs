e31586c69697df23bf720f92149cf585a2a1cfad
Tybo Vanderstraeten <vanderstraeten.tybo@hotmail.com>
2021-01-11 14:03:42 +0100

    feat: Add IGenericRepositoryAsync

---
+++ CobreezeHousehold/Application/Interfaces/IGenericRepositoryAsync.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IGenericRepositoryAsync<T> where T : class
+    {
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<IReadOnlyList<T>> GetPagedResponseAsync(int pageNumber, int pageSize);
+        Task<T> GetByIdAsync(int id);
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<T> DeleteAsync(T entity);
+    }
+}
