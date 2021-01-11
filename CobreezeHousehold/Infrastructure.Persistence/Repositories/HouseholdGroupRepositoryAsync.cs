d9881329a7316c865063d6fad8fa8720fcb77bb8
Tybo Vanderstraeten <vanderstraeten.tybo@hotmail.com>
2021-01-11 17:06:04 +0100

    feat: Implement hosueholdgrouprepositoryasync in persistence

---
+++ CobreezeHousehold/Infrastructure.Persistence/Repositories/HouseholdGroupRepositoryAsync.cs
@@ -0,0 +1,44 @@
+﻿using Application.Interfaces.Repositories;
+using Domain.Entities;
+using Infrastructure.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence.Repositories
+{
+    public class HouseholdGroupRepositoryAsync : GenericRepositoryAsync<HouseholdGroup>, IHouseholdGroupRepositoryAsync
+    {
+        private readonly DbSet<HouseholdGroup> _householdGroups;
+
+        public HouseholdGroupRepositoryAsync(ApplicationDbContext dbContext) : base(dbContext)
+        {
+            _householdGroups = dbContext.Set<HouseholdGroup>();
+        }
+
+        public async Task<IReadOnlyList<User>> GetAllUsersByHouseholdIdAsync(int id, CancellationToken cancellationToken = default)
+        {
+            HouseholdGroup household = await _householdGroups
+                             .FindAsync(new object[] { id }, cancellationToken);
+
+            return (IReadOnlyList<User>)household?.Members;
+        }
+
+        public async Task<IReadOnlyList<HouseholdTask>> GetAllTasksByHouseholdIdAsync(int id, CancellationToken cancellationToken = default)
+        {
+            HouseholdGroup household = await _householdGroups
+                             .FindAsync(new object[] { id }, cancellationToken);
+
+            return (IReadOnlyList<HouseholdTask>)household?.Tasks;
+        }
+
+        public async Task<IReadOnlyList<ShoppingList>> GetAllShoppingListsByHouseholdIdAsync(int id, CancellationToken cancellationToken = default)
+        {
+            HouseholdGroup household = await _householdGroups
+                  .FindAsync(new object[] { id }, cancellationToken);
+
+            return (IReadOnlyList<ShoppingList>)household?.ShoppingsLists;
+        }
+    }
+}
