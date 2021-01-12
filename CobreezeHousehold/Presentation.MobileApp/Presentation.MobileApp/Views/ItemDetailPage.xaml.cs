618c58886b5c95e11250dc6297f0a33c26eac583
Tybo Vanderstraeten <vanderstraeten.tybo@hotmail.com>
2021-01-12 15:45:52 +0100

    dist: Add Xamarin.Forms project to presentation

---
+++ CobreezeHousehold/Presentation.MobileApp/Presentation.MobileApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Presentation.MobileApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Presentation.MobileApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ CobreezeHousehold/Presentation.MobileApp/Presentation.MobileApp/Views/ItemsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Presentation.MobileApp.Models;
+using Presentation.MobileApp.ViewModels;
+using Presentation.MobileApp.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Presentation.MobileApp.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
