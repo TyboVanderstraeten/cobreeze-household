3932935315570dacbbdc21a42b470926478c0f50
Tybo Vanderstraeten <vanderstraeten.tybo@hotmail.com>
2021-01-19 15:50:53 +0100

    feat: Add interface collection get methods

---
+++ CobreezeHousehold/Application/Interfaces/Repositories/IShoppingListRepositoryAsync.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.Repositories
+{
+    public interface IShoppingListRepositoryAsync
+    {
+        Task<IReadOnlyList<ShoppingListItem>> GetAllShoppingListItemsByShoppingListIdAsync(int id, CancellationToken cancellationToken = default);
+        Task<ShoppingListItem> GetShoppingListItemByIdAsync(int id, CancellationToken cancellationToken = default);
+    }
+}
