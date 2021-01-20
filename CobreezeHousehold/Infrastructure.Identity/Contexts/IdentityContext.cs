40a72a3fa315e07b5fec5d621c78f5edda66a63f
Tybo Vanderstraeten <vanderstraeten.tybo@hotmail.com>
2021-01-20 16:49:28 +0100

    feat: Add identitycontext

---
+++ CobreezeHousehold/Infrastructure.Identity/Contexts/IdentityContext.cs
@@ -0,0 +1,11 @@
+﻿using Infrastructure.Identity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Identity.Contexts
+{
+    public class IdentityContext : IdentityDbContext<ApplicationUser>
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> options) : base(options) { }
+    }
+}
