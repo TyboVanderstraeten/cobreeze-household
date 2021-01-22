0965c155217c825433bbf0f3e21fe23a1e7781d0
Tybo Vanderstraeten <vanderstraeten.tybo@hotmail.com>
2021-01-22 18:17:29 +0100

    feat: Add application identity dtos and services

---
+++ CobreezeHousehold/Application/DTOs/Account/RegisterRequest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTOs.Account
+{
+    public class RegisterRequest
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        public string Nickname { get; set; }
+
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        public string PasswordConfirmation { get; set; }
+    }
+}
